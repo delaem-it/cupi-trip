2849de297f15d5077bcd15848c24e66e0773bd43
bmangust <ftf.pollux@gmail.com>
2022-12-28 05:47:52 +0300

    TRIP-54 update FilterCompanies

--- front/src/components/Filter/FilterCompanies/FilterCompanies.tsx
+++ front/src/components/Filter/FilterCompanies/FilterCompanies.tsx
@@ -1,30 +1,59 @@
 import { FunctionComponent } from 'react';
 import CheckboxWIthLogo from '../../CheckboxWIthLogo';
+import Checkbox from '../../Input/Checkbox/Checkbox';
+import Input from '../../Input/Input';
+import FilterItem from '../FilterItem/FilterItem';
 import styles from './FilterCompanies.module.scss';
 
 const FilterCompanies: FunctionComponent = () => {
   return (
-    <div className={styles.filter}>
-      <div className={styles.nameFilter}>
-        <div className={styles.div}>Авиакомпании</div>
-        <img className={styles.icons16px} alt="" src="../icons/icons-16px11.svg" />
-      </div>
-      <div className={styles.filterLists}>
+    <FilterItem name="Авиакомпании">
+      <div className={styles.container}>
         <div className={styles.list}>
-          <CheckboxWIthLogo logo="../icons/aeroPobeda.svg" label="Победа" />
-          <CheckboxWIthLogo logo="../icons/logo1.svg" label="S7 Airlines" />
-          <CheckboxWIthLogo logo="../icons/aeroNordwind.svg" label="Nordwind" />
-          <CheckboxWIthLogo logo="../icons/logo3.svg" label="Turkish airlines" />
-          <CheckboxWIthLogo logo="../icons/aeroaeroSAS.svg" label="SAS" />
-          <CheckboxWIthLogo logo="../icons/logo5.svg" label="Qatar Airways" />
-          <CheckboxWIthLogo logo="../icons/logo6.svg" label="Lufthansa" />
+          <Input type="checkbox" icon={<img src="../icons/aeroPobeda.svg" alt="" />} label="Победа" name="Pobeda" />
+          <Input
+            type="checkbox"
+            icon={<img src="../icons/aeroS7.svg" alt="" />}
+            label="S7 Airlines"
+            name="S7 Airlines"
+          />
+          <Input
+            type="checkbox"
+            icon={<img src="../icons/aeroNordwind.svg" alt="" />}
+            label="Nordwind"
+            name="Nordwind"
+          />
+          <Input
+            type="checkbox"
+            icon={<img src="../icons/aeroTurkishAirlines.svg" alt="" />}
+            label="Turkish airlines"
+            name="Turkish airlines"
+          />
+          <Input type="checkbox" icon={<img src="../icons/aeroSAS.svg" alt="" />} label="SAS" name="SAS" />
+          <Input
+            type="checkbox"
+            icon={<img src="../icons/aeroQatarAirways.svg" alt="" />}
+            label="Qatar Airways"
+            name="Qatar Airways"
+          />
+          <Input
+            type="checkbox"
+            icon={<img src="../icons/aeroLufthansa.svg" alt="" />}
+            label="Lufthansa"
+            name="Lufthansa"
+          />
         </div>
-        <div className={styles.slider}>
-          <div className={styles.rectangleDiv} />
-        </div>
+        <div className={styles.shade} />
       </div>
-      <div className={styles.rectangleDiv1} />
-    </div>
+    </FilterItem>
+
+    // <div className={styles.filter}>
+    //   <div className={styles.nameFilter}>
+    //     <div className={styles.div}>Авиакомпании</div>
+    //     <img className={styles.icons16px} alt="" src="../icons/icons-16px11.svg" />
+    //   </div>
+    //   <div className={styles.rectangleDiv1} />
+    // </div>
   );
 };
 
